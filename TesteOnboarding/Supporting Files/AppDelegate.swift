08d1ad2c09d6bafc1dd9c097cb15f9f5409db8a7
Marcos Tanaka <marcos.tanaka@db1.com.br>
2018-09-26 11:45:14 -0300

    Converting to Swift 4.2

--- TesteOnboarding/Supporting Files/AppDelegate.swift
+++ TesteOnboarding/Supporting Files/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  TesteOnboarding
-//
-//  Created by Marcos Tanaka on 05/08/2018.
-//  Copyright © 2018 Marcos Tanaka. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -13,7 +5,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
